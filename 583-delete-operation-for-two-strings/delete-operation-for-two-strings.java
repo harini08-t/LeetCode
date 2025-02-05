3a9a3334c49a9cc7e94b9a77bae99b8a7f97fcd5
Harini <harinithangavel0810@gmail.com>
2025-02-05 12:09:02 +0530

    Time: 7 ms (77.31%) | Memory: 44.8 MB (68.35%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -3,15 +3,19 @@ class Solution {
         int m = word1.length();
         int n = word2.length();
         int arr[][]=new int[m+1][n+1];
+        for(int i=0;i<=n;i++){
+            arr[0][i]=i;
+
+        }
         for(int i=0;i<=m;i++){
-            for(int j=0;j<=n;j++){
-                if(i==0){
-                    arr[i][j]=j;
-                }
-                else if(j==0){
-                    arr[i][j]=i; 
-                   }
-                    else if(word1.charAt(i-1) == word2.charAt(j-1)){
+            arr[i][0]=i;
+
+        }
+
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                 if(word1.charAt(i-1) == word2.charAt(j-1)){
                     arr[i][j]=arr[i-1][j-1];
                 }
                 else{
